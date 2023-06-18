223355ab6b9da803143b0fbba7f3fa19ddcaf913
Dina <dinagiacobbi@gmail.com>
2023-06-18 09:40:57 -0800

    Creating simple event listener

--- code.js
+++ code.js
@@ -1,2 +1,8 @@
 let textArea = document.getElementById("text");
 let results = document.getElementById("results");
+
+function analyzeMe(event) {
+    console.log(event.target.value)   
+}
+
+textArea.addEventListener('keyup', analyzeMe)
